45b9761e8dcaa5e76a82a1a2f8570f47e8f81a74
Mehmet Berkan <mehmetberkangorel@gmail.com>
2023-10-07 04:28:15 +0300

    Initial commit

---
+++ Simple-Spring-MongoDB-Docker-Example/src/main/java/com/mehmetberkan/Simple/Spring/MongoDB/Docker/Example/SimpleSpringMongoDbDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.Simple.Spring.MongoDB.Docker.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringMongoDbDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringMongoDbDockerExampleApplication.class, args);
+	}
+
+}
---
+++ Simple-Spring-MongoDB-Docker-Example/src/test/java/com/mehmetberkan/Simple/Spring/MongoDB/Docker/Example/SimpleSpringMongoDbDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.Simple.Spring.MongoDB.Docker.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringMongoDbDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
